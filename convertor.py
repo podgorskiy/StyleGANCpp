b7a21c11cee56b4208bbbd178a40c349fa0e33d9
Stanislav Pidhorskyi <stanislav@podgorskiy.com>
2019-12-05 05:22:18 -0500

    Initial

---
+++ convertor.py
@@ -0,0 +1,192 @@
+# Copyright 2019 Stanislav Pidhorskyi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import sys
+sys.path.append('stylegan')
+sys.path.append('stylegan/dnnlib')
+sys.path.append('tensor4/tensor4')
+
+import struct
+import os
+import sys
+import argparse
+import logging
+import torch
+import dnnlib
+import dnnlib.tflib
+import dnnlib.tflib as tflib
+import pickle
+import numpy as np
+from torchvision.utils import save_image
+import PIL
+from dnnlib import EasyDict
+import var_table
+
+
+def write_blob(model, filename=None):
+    if filename is None:
+        filename = model.class_name + '.bin'
+
+    with open(filename, "wb") as f:
+        for w_name in model.model_dict.keys():
+            w = model.model_dict[w_name]
+            f.write(w_name.encode())
+            f.write(bytes([0]))
+            if w.dtype == np.float64:
+                f.write(bytes(b'doubl'))
+            if w.dtype == np.float32:
+                f.write(bytes(b'float'))
+            if w.dtype == np.int32:
+                f.write(bytes(b'int32'))
+            if w.dtype == np.int16:
+                f.write(bytes(b'int16'))
+            f.write(struct.pack("b", w.ndim))
+            for i in range(w.ndim):
+                f.write(struct.pack("i", w.shape[i]))
+            f.write(w.tobytes())
+
+
+class Model:
+    def __init__(self, name):
+        self.class_name = name
+        self.model_dict = {}
+
+
+def load_from(name):
+    dnnlib.tflib.init_tf()
+    with open(name, 'rb') as f:
+        m = pickle.load(f)
+
+    Gs = m[2]
+
+    model = Model('StyleGAN')
+
+    def tensor(x, transpose=None):
+        x = Gs.vars[x].eval()
+        if transpose:
+            x = np.transpose(x, transpose)
+        return x
+
+    for i in range(8):
+        w = tensor('G_mapping/Dense%d/weight' % i, (1, 0))
+        b = tensor('G_mapping/Dense%d/bias' % i)
+        gain = np.sqrt(2.0)
+        lrmul = 0.01
+        std = gain / np.sqrt(w.shape[1]) * lrmul
+        model.model_dict["mapping_block_%d_weight" % (i + 1)] = w * std
+        model.model_dict["mapping_block_%d_bias" % (i + 1)] = b * lrmul
+
+    model.model_dict["dlatent_avg"] = tensor('dlatent_avg')
+    model.model_dict["block_0_const"] = tensor('G_synthesis/4x4/Const/const')
+
+    rnd = np.random.RandomState(5)
+    latents = rnd.randn(1, 512)
+    model.model_dict["latents"] = latents.astype(np.float32)
+
+    for i in range(9):
+        j = 9 - i - 1
+        name = '%dx%d' % (2 ** (2 + i), 2 ** (2 + i))
+
+        prefix = 'G_synthesis/%s' % name
+
+        if i == 0:
+            prefix_1 = '%s/Const' % prefix
+            prefix_2 = '%s/Conv' % prefix
+        else:
+            prefix_1 = '%s/Conv0_up' % prefix
+            prefix_2 = '%s/Conv1' % prefix
+
+        model.model_dict["block_%d_noise_weight_1" % i] = tensor('%s/Noise/weight' % prefix_1)[None, :, None, None]
+        model.model_dict["block_%d_noise_weight_2" % i] = tensor('%s/Noise/weight' % prefix_2)[None, :, None, None]
+
+        if i != 0:
+            if 2 ** (2 + i) >= 128:
+                w = tensor('%s/weight' % prefix_1, (2, 3, 0, 1))
+                gain = np.sqrt(2.0)
+                std = gain / np.sqrt(np.prod([w.shape[0]] + list(w.shape[2:])))
+                w = np.pad(w, ((0, 0), (0, 0), (1, 1), (1, 1)), mode='constant')
+                w = w[:, :, 1:, 1:] + w[:, :, :-1, 1:] + w[:, :, 1:, :-1] + w[:, :, :-1, :-1]
+                model.model_dict["block_%d_conv_1_weight" % i] = w * std
+            else:
+                w = tensor('%s/weight' % prefix_1, (3, 2, 0, 1))
+                gain = np.sqrt(2.0)
+                std = gain / np.sqrt(np.prod(w.shape[1:]))
+                model.model_dict["block_%d_conv_1_weight" % i] = w * std
+
+        gain = np.sqrt(2.0)
+        w = tensor('%s/weight' % prefix_2, (3, 2, 0, 1))
+        std = gain / np.sqrt(np.prod(w.shape[1:]))
+        model.model_dict["block_%d_conv_2_weight" % i] = w * std
+
+        model.model_dict["block_%d_bias_1" % i] = tensor('%s/bias' % prefix_1)[None, :, None, None]
+        model.model_dict["block_%d_bias_2" % i] = tensor('%s/bias' % prefix_2)[None, :, None, None]
+
+        gain = 1
+        w = tensor('%s/StyleMod/weight' % prefix_1, (1, 0))
+        std = gain / np.sqrt(np.prod(w.shape[1]))
+        model.model_dict["block_%d_style_1_weight" % i] = w * std
+        model.model_dict["block_%d_style_1_bias" % i] = tensor('%s/StyleMod/bias' % prefix_1)
+
+        gain = 1
+        w = tensor('%s/StyleMod/weight' % prefix_2, (1, 0))
+        std = gain / np.sqrt(np.prod(w.shape[1]))
+        model.model_dict["block_%d_style_2_weight" % i] = w * std
+        model.model_dict["block_%d_style_2_bias" % i] = tensor('%s/StyleMod/bias' % prefix_2)
+
+        gain = 1
+        w = tensor('G_synthesis/ToRGB_lod%d/weight' % (j), (3, 2, 0, 1))
+        std = gain / np.sqrt(np.prod(w.shape[1]))
+        model.model_dict["block_%d_to_rgb_weight" % i] = w * std
+        model.model_dict["block_%d_to_rgb_bias" % i] = tensor('G_synthesis/ToRGB_lod%d/bias' % (j))
+
+    return model #, Gs_
+
+
+if __name__ == '__main__':
+    model = load_from('karras2019stylegan-ffhq-1024x1024.pkl')
+    write_blob(model)
+
+    def write_h(x, *args):
+        source_h.write(x % args)
+
+    def write_cpp(x, *args):
+        source_cpp.write(x % args)
+
+    with open(model.class_name + ".cpp", "w") as source_cpp, open(model.class_name + ".h", "w") as source_h:
+        write_h('#include "tensor4.h"' + '\n' * 3)
+        write_cpp('#include "%s"' % (model.class_name + ".h") + '\n' * 3)
+
+        write_h('struct %s\n{\n' % model.class_name)
+
+        arguments = []
+
+        for var_name, var in model.model_dict.items():
+            var_type = 't4::tensor%df' % len(var.shape)
+            decl_str = "\t%s %s;\n" % (var_type, var_name)
+            write_h(decl_str)
+
+        write_h('};' + '\n' * 3)
+
+        declaration = '%s %sLoad(const char* filename)' % (model.class_name, model.class_name)
+        write_h(declaration + ";\n\n")
+        write_cpp(declaration + "\n{\n")
+        write_cpp('\t%s ctx;\n', model.class_name)
+        write_cpp('\tt4::model_dict dict = t4::load(filename);\n')
+
+        for var_name, var in model.model_dict.items():
+            string = "\tdict.load(ctx.%s, \"%s\", %s);\n" % (var_name, var_name, ', '.join([str(p) for p in var.shape]))
+            write_cpp(string)
+
+        write_cpp('\treturn ctx;\n}' + '\n' * 3)
---
+++ main.cpp
@@ -0,0 +1,493 @@
+#define T4_USE_OMP 1
+//#define USE_MKLDNN
+#include "tensor4.h"
+#include "StyleGAN.h"
+#include "image_io.h"
+
+
+t4::tensor2f MappingForward(const StyleGAN& model, t4::tensor2f x)
+{
+	x = t4::Linear(x, model.mapping_block_1_weight, model.mapping_block_1_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_2_weight, model.mapping_block_2_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_3_weight, model.mapping_block_3_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_4_weight, model.mapping_block_4_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_5_weight, model.mapping_block_5_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_6_weight, model.mapping_block_6_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_7_weight, model.mapping_block_7_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	x = t4::Linear(x, model.mapping_block_8_weight, model.mapping_block_8_bias);
+	x = t4::LeakyReluInplace(x, 0.2);
+	return x;
+}
+
+
+t4::tensor4f IN(t4::tensor4f x)
+{
+	auto e = t4::GlobalAveragePool2d(x);
+	x = x - e;
+	auto var = t4::GlobalAveragePool2d(x * x);
+	return x / t4::Pow(var + 1e-8f, 0.5f);
+}
+
+
+t4::tensor4f style_mod(t4::tensor4f x, t4::tensor2f style)
+{
+	t4::tensor1i shape = t4::Shape(style);
+	t4::tensor1i count = t4::Unsqueeze<0>(t4::Gather(shape, t4::Constant<t4::int64>(0)));
+	t4::tensor1i component = t4::Unsqueeze<0>(t4::Gather(shape, t4::Constant<t4::int64>(1)));
+	component = component / 2L;
+	t4::tensor1i chunks = t4::Unsqueeze<0>(t4::Constant<t4::int64>(2));
+	t4::tensor1i _shape = t4::Concat<0>(t4::Concat<0>(count, chunks), component);
+	auto _style = t4::Unsqueeze<3>(t4::Unsqueeze<3>(t4::Reshape<3>(style, _shape)));
+	t4::tensor4f style_1 = t4::tensor4f::New({_style.shape()[0], _style.shape()[2], 1, 1});
+	t4::tensor4f style_2 = t4::tensor4f::New({_style.shape()[0], _style.shape()[2], 1, 1});
+	for (int i = 0; i < _style.shape()[0]; ++i)
+	{
+		style_1.Sub(i).Assign(_style.Sub(i, 0));
+		style_2.Sub(i).Assign(_style.Sub(i, 1));
+	}
+	return style_2 + x * (style_1 + 1.0f);
+}
+
+t4::tensor4f blur2d(t4::tensor4f in)
+{
+	const int N = number(in);
+	const int C = channels(in);
+	const int H = height(in);
+	const int W = width(in);
+
+	in = t4::Pad<t4::constant>(in, 0, 0, 1, 1, 0, 0, 1, 1);
+	const int _W = W + 2;
+
+	t4::tensor4f out = t4::tensor4f::New({ N, C, H, W });
+
+	for (int n = 0; n < N; ++n)
+	{
+		parallel_for(int c = 0; c < C; ++c)
+		{
+			auto inSubtensor = in.Sub(n, c);
+			const float* __restrict src = inSubtensor.ptr();
+			auto outSubtensor = out.Sub(n, c);
+			float* __restrict dst = outSubtensor.ptr();
+
+			for (int i = 0; i < H; i++)
+			{
+				for (int j = 0; j < W; j++)
+				{
+					float v = 0;
+					i += 1;
+					j += 1;
+					v += src[(i + 0) * _W + j + 0] * 4;
+					v += src[(i + 0) * _W + j - 1] * 2;
+					v += src[(i + 0) * _W + j + 1] * 2;
+					v += src[(i - 1) * _W + j + 0] * 2;
+					v += src[(i + 1) * _W + j + 0] * 2;
+					v += src[(i - 1) * _W + j - 1];
+					v += src[(i + 1) * _W + j - 1];
+					v += src[(i - 1) * _W + j + 1];
+					v += src[(i + 1) * _W + j + 1];
+					i -= 1;
+					j -= 1;
+					v /= (4 + 2 * 4 + 4);
+					dst[i * W + j] = v;
+				}
+			}
+		}
+	}
+	return out;
+}
+
+
+t4::tensor4f updcale2d(t4::tensor4f in)
+{
+	const int N = number(in);
+	const int C = channels(in);
+	const int Hin = height(in);
+	const int Win = width(in);
+
+	const int Hout = 2 * Hin;
+	const int Wout = 2 * Win;
+
+	t4::tensor4f out = t4::tensor4f::New({ N, C, Hout, Wout });
+
+	for (int n = 0; n < N; ++n)
+	{
+		parallel_for(int c = 0; c < C; ++c)
+		{
+			auto inSubtensor = in.Sub(n, c);
+			const float* __restrict src = inSubtensor.ptr();
+			auto outSubtensor = out.Sub(n, c);
+			float* __restrict dst = outSubtensor.ptr();
+
+			for (int i = 0; i < Hin; i++)
+			{
+				for (int j = 0; j < Win; j++)
+				{
+					dst[(2 * i + 0) * Wout + 2 * j + 0] = src[i * Win + j];
+					dst[(2 * i + 0) * Wout + 2 * j + 1] = src[i * Win + j];
+					dst[(2 * i + 1) * Wout + 2 * j + 0] = src[i * Win + j];
+					dst[(2 * i + 1) * Wout + 2 * j + 1] = src[i * Win + j];
+				}
+			}
+		}
+	}
+	return out;
+}
+
+
+int main()
+{
+	StyleGAN model = StyleGANLoad("StyleGAN.bin");
+
+	auto z = t4::tensor2f::RandN({1, 512});
+	//auto z = model.latents;
+	float s = 0;
+	const float* __restrict src = z.ptr();
+	for (int64_t i = 0, l = z.size(); i < l; ++i)
+	{
+		float x = src[i] * src[i];
+		s += x;
+	}
+	s /= z.size();
+
+    z = z / sqrt(s + 1e-8f);
+
+	auto w = MappingForward(model, z);
+
+	w = (w - t4::Unsqueeze<0>(model.dlatent_avg)) * 0.7f + t4::Unsqueeze<0>(model.dlatent_avg);
+
+	auto x = model.block_0_const;
+
+#define BLOCK(X) model.block_0_##X
+
+	{
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_1_##X
+	{
+		x = updcale2d(x);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_2_##X
+	{
+		x = updcale2d(x);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_3_##X
+	{
+		x = updcale2d(x);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_4_##X
+	{
+		x = updcale2d(x);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_5_##X
+	{
+		x = t4::ConvTranspose2d<4, 4, 2, 2, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_6_##X
+	{
+		x = t4::ConvTranspose2d<4, 4, 2, 2, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+
+
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_7_##X
+	{
+		x = t4::ConvTranspose2d<4, 4, 2, 2, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+
+
+	//////////////////
+#undef 	BLOCK
+#define BLOCK(X) model.block_8_##X
+	{
+		x = t4::ConvTranspose2d<4, 4, 2, 2, 1, 1, 1, 1>(x, BLOCK(conv_1_weight));
+
+		x = blur2d(x);
+
+		x = x + BLOCK(noise_weight_1) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_1);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s1 = t4::Linear(w, BLOCK(style_1_weight), BLOCK(style_1_bias));
+
+		x = style_mod(x, s1);
+
+		x = t4::Conv2d<3, 3, 1, 1, 1, 1, 1, 1>(x, BLOCK(conv_2_weight));
+
+		x = x + BLOCK(noise_weight_2) * t4::tensor4f::RandN({x.shape()[0], 1, x.shape()[2], x.shape()[3]});
+
+		x = x + BLOCK(bias_2);
+
+		x = t4::LeakyReluInplace(x, 0.2);
+
+		x = IN(x);
+
+		auto s2 = t4::Linear(w, BLOCK(style_2_weight), BLOCK(style_2_bias));
+
+		x = style_mod(x, s2);
+	}
+
+
+	x = t4::Conv2d<1, 1, 1, 1, 0, 0, 1, 1>(x, BLOCK(to_rgb_weight), BLOCK(to_rgb_bias));
+
+	image_io::imwrite(x.Sub(0) * 0.5f + 0.5f, "image.png");
+
+	std::cout << t4::Shape(x);
+
+	//std::cout << x;
+	return 0;
+}
